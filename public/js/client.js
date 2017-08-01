0f53f84d9609d7f7d63a17f64e79fc3c978784c6
Easonwang01 <easonwang01>
2017-08-01 09:36:24 +0800

    add

--- public/js/client.js
+++ public/js/client.js
@@ -81,7 +81,7 @@ function timeFormat(msTime) {
 
 $(document).ready(function() {
   //setup "global" variables first
-  var socket = io.connect("https://chat.sakatu.com/");
+  var socket = io.connect();
   var myRoomID = null;
 
   $("form").submit(function(event) {
