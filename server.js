2a66f9e0090d881cbec909a25e9922f63e92ec82
Easonwang01 <easonwang01>
2017-08-01 09:30:16 +0800

    add

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express')
 
 app.configure(function() {
 	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3005);
-  	app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+  app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(express.static(__dirname + '/public'));
@@ -26,7 +26,7 @@ app.get('/', function(req, res) {
   res.render('index.html');
 });
 
-server.listen(app.get('port'), app.get('ipaddr'), function(){
+server.listen(app.get('port'), function(){
 	console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
 });
 
